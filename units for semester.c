226ecbc94de0e3c91b3fd5d9700c14d523ffa10d
frakline moni <franklinehijax@gmail.com>
2025-09-25 10:21:22 +0300

    Add files via upload

---
+++ units for semester.c
@@ -0,0 +1,24 @@
+/*
+NAME:frankline osano moni
+ADM:PA106/G/28937/25
+DESCRIPTION:program to displayy units for the semester
+*/
+
+#include<stdio.h>include 
+
+main (){
+	
+	printf("SPM 2100| Discrete math\n");
+	printf("SPM 2104| Math for science\n");
+	printf("UCU 2105|fundamental of ict");
+	printf("\nSPI 2102| Computer system and organization");
+	printf("\nUCU 2101| Communication skills");
+	printf("\nspc 2101| Introduction to programing");
+	printf("\nSSE2101| fundamental of system anaylsis and design\n");
+	
+	
+	
+	
+	
+	return 0;
+}
