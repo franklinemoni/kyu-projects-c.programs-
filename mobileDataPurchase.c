74133290e4e8be357675b7e70bbf5edc7b343065
fraklinemoni <franklinehijax@gmail.com>
2025-10-03 12:48:49 +0300

    Add files via upload

---
+++ mobileDataPurchase.c
@@ -0,0 +1,38 @@
+
+
+#include<stdio.h>
+
+main(){
+	
+	int choice;
+	
+	printf("1. 100mbs@50\n2. 500mbs@200\n3. 1GB@350\n4. 2GB@600");
+	printf("\nEnter your choice:");
+	scanf("%d",&choice);
+	
+	switch (choice){
+		case 1:
+		printf("100mbs@50");
+		break;	
+	
+	case 2:
+		printf("500mbs@20");
+		break;
+	
+	case 3:
+		printf("1gb@350");
+		break;
+	
+	case 4:
+		printf("2GB@600");
+		break;
+	default:
+	 printf("invalid");
+	}
+
+	
+	
+	
+	
+	return 0;
+}
