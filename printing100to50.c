d0723e7dd9d258d0256b444ce2d10d7b83e6e427
fraklinemoni <franklinehijax@gmail.com>
2025-10-04 11:42:41 +0300

    Add files via upload

---
+++ printing100to50.c
@@ -0,0 +1,19 @@
+/*name:frankline moni
+  adm:PA106/G/28937/25
+  DESCRIPION:printing numbers from 100 down to 50
+  */
+
+#include<stdio.h>
+
+int main(){
+
+    int i=100;
+
+    for(i=100;i>=50;i--){
+
+
+        printf("%d\n",i);
+    }
+
+return 0;
+}
